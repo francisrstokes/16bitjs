8e59199a84a916a00816ade6631fa66eaf1e6754
Frank Stokes <frank@wearereasonablepeople.com>
2017-07-10 17:11:28 +0200

    Simplified debgger

--- src/debugger/render.js
+++ src/debugger/render.js
@@ -1,6 +1,27 @@
-const logState = require('./log-state');
+const {
+  leftPad,
+  arrayAsHex,
+  splitInstruction
+} = require('../utils');
+const { INSTRUCTION_MAP } = require('../constants');
+
+const memory = require('../memory/memory');
+const stack = require('../memory/stack');
+const registers = require('../cpu/registers')
 
 module.exports = () => {
-  logState();
+  const instruction = memory[registers.IP];
+  const opcode = splitInstruction(instruction)[0];
+  const namedOpcode = INSTRUCTION_MAP[opcode];
+
+  const regs = Object.keys(registers)
+    .reduce((outputStr, registerName) =>
+      `${outputStr}${registerName}: ${leftPad(registers[registerName].toString(16))}\t`,
+    '');
+  console.log(`Memory:\n${arrayAsHex(memory)}\n`);
+  console.log(`Stack:\n${arrayAsHex(stack)}\n`);
+  console.log(`Instruction: (${namedOpcode}) ${leftPad(instruction.toString(2), 16)}`);
+  console.log(`Registers:\n${regs}\n`);
+
   process.stdout.write('(s)tep (e)xit >>> ');
 };
