96707ad767bdd1c04ecaafe37bf4ffbb46418bee
Frank Stokes <frank@wearereasonablepeople.com>
2017-07-24 16:27:46 +0200

    Extracts comments in a way that allows data section to have semicolons.
    With the caveat that those semicolons need to be escaped in the declaration

--- src/assembler/preprocessor/stages/cleanup.js
+++ src/assembler/preprocessor/stages/cleanup.js
@@ -1,6 +1,19 @@
+const commentRegex = /(?!\\).;/g;
+
 const removeWhitespace = line => line.trim();
-const removeComments = line => line.split(';')[0].trim();
 const removeEmptyLines = line => line !== '';
+const removeComments = (line) => {
+  if (line[0] === ';') return '';
+  const commentMatches = line.match(commentRegex);
+  if (commentMatches && commentMatches.length > 0) {
+    const commentIndex = commentMatches[0][0] === ';'
+      ? line.indexOf(commentMatches[0])
+      : line.indexOf(commentMatches[0]) + 1;
+
+    return line.substring(0, commentIndex).trim();
+  }
+  return line;
+};
 
 module.exports = (file) =>
   file
