e0ab89722f258aea18035185aec230d6a28213a7
Frank Stokes <frank@wearereasonablepeople.com>
2017-07-26 16:10:22 +0200

    Fixes issue #22

--- src/assembler/preprocessor/stages/pseudo-instructions/expanders/general/LDV16.js
+++ src/assembler/preprocessor/stages/pseudo-instructions/expanders/general/LDV16.js
@@ -1,14 +1,18 @@
-const { getInstructionArguments } = require('../../../../utils');
+const {
+  getInstructionArguments,
+  getUsableRegister
+} = require('../../../../utils');
 
 module.exports = (instruction) => {
   const [register, value] = getInstructionArguments(instruction);
+  const mutableRegister = getUsableRegister(register);
   return [
     `LDV ${register}, (${value} >> 8)`,
     `LSF ${register}, 4`,
     `LSF ${register}, 4`,
-    'PSH B',
-    `LDV B, (${value} & 0b0000000011111111)`,
+    `PSH ${mutableRegister}`,
+    `LDV ${mutableRegister}, (${value} & 0b0000000011111111)`,
     `ADD ${register}, B`,
-    'POP B'
+    `POP ${mutableRegister}`
   ];
 };
