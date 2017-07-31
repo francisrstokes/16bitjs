b7e509aa4e33f1454a5ea2deaff600dbf79dbb7b
Frank Stokes <frank@wearereasonablepeople.com>
2017-07-31 21:49:18 +0200

    Strips potential malcious characters before passing to eval()

--- src/assembler/preprocessor/stages/evaluate-expressions.js
+++ src/assembler/preprocessor/stages/evaluate-expressions.js
@@ -6,7 +6,8 @@ module.exports = (instructions) => {
     if (expressions) {
       return expressions
         .reduce((acc, expr) => {
-          const evaluated = eval(expr);
+          const safeEval = expr.replace(/[^0-9xa-fA-F.|*&^%+\-<>/\s]/g, '');
+          const evaluated = eval(safeEval);
           return acc.replace(expr, evaluated);
         }, instruction);
     }
