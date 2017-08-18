6407e8bc7abdab36066a6930dd40bd779622d154
James Tancock <james.tancock@momentumft.co.uk>
2017-08-18 11:21:19 +0100

    Remove extra indentation in program-loader

--- src/program-loader/index.js
+++ src/program-loader/index.js
@@ -2,12 +2,12 @@ const { fs, convertUint8ArrayToUint16Array } = require('../utils');
 const sanityCheck = require('./sanity-check');
 
 module.exports = (argv, memory) =>
-    sanityCheck(argv)
-      .then(() => fs.readFileAsync(argv.p))
-      .then(result => new Uint8Array(result))
-      .then(convertUint8ArrayToUint16Array)
-      .then(program => {
-        program.forEach((v, i) => {
-          memory.memory[i] = v;
-        })
+  sanityCheck(argv)
+    .then(() => fs.readFileAsync(argv.p))
+    .then(result => new Uint8Array(result))
+    .then(convertUint8ArrayToUint16Array)
+    .then(program => {
+      program.forEach((v, i) => {
+        memory.memory[i] = v;
       })
+    })
