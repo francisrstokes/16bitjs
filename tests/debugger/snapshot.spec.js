37ee56f38b958101ad08b30417bf6ad280e59836
Onur Gunduz <ogunduz@gmail.com>
2017-10-23 01:06:06 +0200

    Fix debugger tests

--- tests/debugger/snapshot.spec.js
+++ tests/debugger/snapshot.spec.js
@@ -1,26 +1,51 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { assert } = require('chai');
+const fs = require('fs');
+const preprocessor = require('../../src/assembler/preprocessor');
+const assembler = require('../../src/assembler/assembler');
+const { encode, decode } = require('../../src/debugger/snapshot');
+const pagedMemory = require('../../src/debugger/memory-pages');
+const { arrayAsHex, leftPad } = require('../../src/utils');
+const $memory = require('../../src/memory');
+const { DEBUG, MEM_SIZE, STACK_SIZE } = require('../../src/constants');
+const memory = $memory.memory;
+const cpu = require('../../src/cpu')($memory);
+const registers = require('../../src/cpu/registers');
 
-const browserify = require('browserify');
-const vm = require('vm');
-const brfs = require('brfs');
+const stack = $memory.stack(registers).raw;
 
-describe('snapshot', () => {
-  it('encode/decode xor-swap', done => {
-    const b = browserify()
-    b.add(__dirname + '/snapshot.js');
-    b.transform(brfs);
-    b.bundle((err, src) => {
-      if (err) {
-        throw err;
-      }
+describe('debugger', () => {
+  it('encode/decode', () => {
+    assembler(preprocessor(fs.readFileSync(__dirname + '/../../asm/factorial.asm', 'utf8')))
+      .forEach((v, i) => {
+        memory[i] = v;
+      });
 
-      const assert = (a, b) => {
-        expect(a).to.deep.equal(b);
-      }
+    while (!cpu.step()) {
+      const res = decode(encode(0));
+      assert.deepEqual(res.registers, registers);
+      assert.deepEqual(res.stack, stack);
+      const pageSize = MEM_SIZE / DEBUG.NUM_PAGES;
+      assert.deepEqual(res.memory, memory.slice(0, pageSize));
+    }
 
-      vm.runInNewContext(src, { done, assert, console: { log: () => { } } })
+    memory.fill(0, 0, MEM_SIZE)
+    stack.fill(0, 0, STACK_SIZE)
 
+    Object.keys(registers).forEach(key => {
+      registers[key] = 0
     });
+
+    assembler(preprocessor(fs.readFileSync(__dirname + '/../../asm/xor-swap.asm', 'utf8')))
+      .forEach((v, i) => {
+        memory[i] = v;
+      });
+
+    while (!cpu.step()) {
+      const res = decode(encode(0));
+      assert.deepEqual(res.registers, registers);
+      assert.deepEqual(res.stack, stack);
+      const pageSize = MEM_SIZE / DEBUG.NUM_PAGES;
+      assert.deepEqual(res.memory, memory.slice(0, pageSize));
+    }
   });
 })
