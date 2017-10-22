a833222a3d597a6df5b5a8f67e23843e7a8cfb9b
Onur Gunduz <ogunduz@gmail.com>
2017-10-22 15:52:13 +0200

    Make sure labelOffset is reset

--- src/assembler/preprocessor/stages/replace-labels.js
+++ src/assembler/preprocessor/stages/replace-labels.js
@@ -1,20 +1,21 @@
-let labelOffset = 0;
-const labels = {};
+module.exports = (instructions) => {
+  const labels = {};
+  let labelOffset = 0;
 
-const extractLabels = (line, i) => {
-  const firstPart = line.split(' ')[0];
-  if (firstPart[firstPart.length - 1] === ':') {
-    labels[line.toLowerCase()] = i - labelOffset++;
-    return false;
-  }
-  return true;
-};
-
-const replaceLabels = (line) => Object.keys(labels)
-  .sort((a, b) => b.length - a.length)
-  .reduce((outLine, label) => outLine.replace(label, labels[label]), line)
-
-module.exports = (instructions) =>
-  instructions
-    .filter(extractLabels)
-    .map(replaceLabels);
+  return instructions
+    .filter((line, i) => {
+      const firstPart = line.split(' ')[0];
+      if (firstPart[firstPart.length - 1] === ':') {
+        labels[line.toLowerCase()] = i - labelOffset++;
+        return false;
+      }
+      return true;
+    })
+    .map(line =>
+      Object.keys(labels)
+        .sort((a, b) => b.length - a.length)
+        .reduce((outLine, label) =>
+          outLine.replace(label, labels[label]),
+        line)
+    );
+}
